8183dfe68af835a90c996520a84605235411b773
junaedsiam <junaedsiam@gmail.com>
2023-05-21 23:40:02 +0600

    pattern triangle added

---
+++ pattern/triangle.py
@@ -0,0 +1,16 @@
+def printTriangle(n):
+    for i in range(n):
+        st = ''
+        for _ in range(n - (i + 1)):
+            st += " "
+        for _ in range(2 * (i + 1) - 1):
+            st += '*'
+        print(st)
+
+
+if __name__ == '__main__':
+    printTriangle(3)
+    printTriangle(4)
+
+    printTriangle(5)
+    printTriangle(6)
