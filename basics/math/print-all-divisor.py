e5dc4cda2b3e774e7b5269a601a42e4bd80ad293
junaedsiam <junaedsiam@gmail.com>
2023-05-23 23:35:31 +0600

    basic math: print all divisor

--- basics/math/print-all-divisor.py
+++ basics/math/print-all-divisor.py
@@ -1,9 +1,29 @@
 # Brute force
-def print_all_divisor(num):
+# Time complexity: O(n / 2 ) -> O(n), Space complexity: O(1)
+def print_all_divisors_brute(num):
+    for i in range(1, num // 2 + 1):
+        if num % i == 0:
+            print(i, end=" ")
+    print(num)
 
-    pass
+# Better approach
+# The quotient of a Dividend by one of the divisors is actually another divisor of that dividend.
+# Like, 4 divides 36. The quotient is 9, and 9 also divides 36
+# And you only have to traverse till sqrt(num) to find all the divisors
+# Time complexity: O(sqrt(N)), Space complexity: O(1)
+
+
+def print_all_divisors(num):
+    for i in range(1, int(num ** 0.5) + 1):
+        if num % i == 0:
+            print(i, end=" ")
+            if i != num // i:
+                print(num//i, end=" ")
+    print()
 
 
 if __name__ == '__main__':
-    print_all_divisor(36)
-    print_all_divisor(11)
+    print_all_divisors_brute(36)
+    print_all_divisors_brute(11)
+    print_all_divisors(36)
+    print_all_divisors(11)
