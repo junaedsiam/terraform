7d601ee51707f1c347f869558650b686ed0e526a
junaedsiam <junaedsiam@gmail.com>
2023-05-25 01:20:06 +0600

    sorting: selection sort doc improvement

--- sorting/selection-sort.py
+++ sorting/selection-sort.py
@@ -1,3 +1,8 @@
+"""
+Selection sort is a simple and efficient sorting algorithm that works by repeatedly selecting the smallest (or largest) element from the unsorted portion of the list and moving it to the sorted portion of the list. 
+"""
+
+
 def selection_sort(nums):
     n = len(nums)
     for i in range(n - 1):
