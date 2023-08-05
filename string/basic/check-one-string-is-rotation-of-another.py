846595f468c6f49c2c240db4372dfcbf140a0185
junaedsiam <junaedsiam@gmail.com>
2023-08-05 18:46:12 +0600

    string: basic: check one string is rotation of another

---
+++ string/basic/check-one-string-is-rotation-of-another.py
@@ -0,0 +1,31 @@
+"""
+Problem description: 
+---------
+Given two strings s and goal, return true if and only if s can become goal after some number of shifts on s.
+A shift on s consists of moving the leftmost character of s to the rightmost position.
+For example, if s = "abcde", then it will be "bcdea" after one shift.
+ 
+Example 1:
+-------
+Input: s = "abcde", goal = "cdeab"
+Output: true
+
+Example 2:
+--------
+Input: s = "abcde", goal = "abced"
+Output: false
+"""
+
+
+def check_one_string_is_rotation_of_another(s, goal):
+    # Time complexity: O(n * log(m))
+    # As the complexity of find and match is n * log m , where n is the length of the whole string
+    # And m is the length of the substring
+    if len(s) != len(goal):
+        return False
+    return (s + s).find(goal) != -1
+
+
+if __name__ == '__main__':
+    ex_1, ex_2 = ("abcde", "cdeab"), ("abcde", "adced")
+    check_one_string_is_rotation_of_another()
