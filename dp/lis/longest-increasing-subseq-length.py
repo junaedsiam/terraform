9a3e979edb46b8bf9e1ba985c339e54a88da49a8
junaedsiam <junaedsiam@gmail.com>
2024-02-18 23:43:09 +0600

    dp: lis: longest increasing subseq regular tabulation added

--- dp/lis/longest-increasing-subseq-length.py
+++ dp/lis/longest-increasing-subseq-length.py
@@ -54,6 +54,20 @@ def lis_memo(nums: List[int]) -> int:
 
 
 def lis_tabulation(nums: List[int]) -> int:
+    n = len(nums)
+    dp = [[0] * (n + 1) for _ in range(n + 1)]
+    for i in range(n - 1, -1, -1):
+        for prev in range(i - 1, -2, -1):
+            not_take = dp[i + 1][prev + 1]
+            take = 0
+            if prev == -1 or nums[i] > nums[prev]:
+                take = 1 + dp[i + 1][i + 1]
+            dp[i][prev + 1] = max(not_take, take)
+
+    return dp[0][0]
+
+
+def lis_tabulation_alter(nums: List[int]) -> int:
     # Time complexity: O(n ^ 2)
     # Space complexity: O(n)
     # ---
@@ -109,4 +123,5 @@ def lis_print_tabulation(nums):
 if __name__ == '__main__':
     print(lis_memo([10, 9, 2, 5, 3, 7, 101, 18]))
     print(lis_tabulation([10, 9, 2, 5, 3, 7, 101, 18]))
+    print(lis_tabulation_alter([10, 9, 2, 5, 3, 7, 101, 18]))
     print(lis_print_tabulation([10, 9, 2, 5, 3, 7, 101, 18]))
